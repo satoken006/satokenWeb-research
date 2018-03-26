d19b07f060f459d5811903eccb42abaeee637391
satoken912 <seat.soil.74196901@gmail.com>
2018-03-26 16:44:37 +0900

    First commit

---
+++ accel/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../../css/style.css">
+    <script src="../../main.js"></script>
+    <title>研究成果</title>
+</head>
+<body>
+    <div class="nav fixed">
+        <ul id="menubar">
+            <li><a href="http://satoken.nkmr.io/">HOME</a></li>
+            <li><a href="http://satoken.nkmr.io/profile/">PROFILE</a></li>
+            <li><a href="http://satoken.nkmr.io/research/">RESEARCH</a></li>
+            <li><a href="http://satoken.nkmr.io/product/">PRODUCT</a></li>
+            <li><a href="http://satoken.nkmr.io/activity/">ACTIVITY</a></li>
+            <li><a href="http://satoken.nkmr.io/link/">LINK</a></li>
+        </ul>
+    </div>
+    <img id="header" src="http://satoken.nkmr.io/img/header.jpg">
+
+    <div id="content">
+        <div class="heading">研究成果 - ノイズ</div>
+
+        <div class="research-category-box">
+            <a href="http://satoken.nkmr.io/research/handwriting/">
+                <img src="../../img/research/handwriting.png">
+                <p>
+                    <span>手書き文字<br>×<br>アニメーション</span>
+                </p>
+            </a>
+        </div>       
+        <div class="research-category-box">
+            <a href="http://satoken.nkmr.io/research/comic/">
+                <img src="../../img/research/comic.png">
+                <p>
+                    <span>コミック<br>読書促進</span>
+                </p>
+            </a>
+        </div>
+        <div class="research-category-box">
+            <a href="http://satoken.nkmr.io/research/accel/">
+                <img src="../../img/research/accel.png">
+                <p>
+                    <span>ノイズ<br>キャンセリング<br>ミュージック</span>
+                </p>
+            </a>
+        </div>        
+        <div class="research-category-box">
+            <a href="http://satoken.nkmr.io/research/memory/">
+                <img src="../../img/research/memory.png">
+                <p>
+                    <span>暗記学習支援</span>
+                </p>
+            </a>
+        </div>
+
+        <?php
+
+        require "../../php/function.php";
+        require "../php/connectToMySQL.php";
+
+        $connection = connect_to_mysql( "satoken_web" );
+        showResearches( "accel" );
+        mysql_close( $connection );
+
+        ?>
+    </div>
+
+</body>
+</html>
---
+++ php/connectToMySQL.php
@@ -0,0 +1,50 @@
+<?php
+
+$connection = connect_to_mysql( "satoken_web" );
+
+function showResearches( $category ){
+    $mode = $_GET["mode"];
+
+    $q = "SELECT * FROM research WHERE category='$category'";
+    $r = mysql_query( $q );
+    while( $c = mysql_fetch_array( $r ) ){
+
+        if( $c[7] == "private" && $mode != "admin" ){
+            continue;
+        }
+
+        $item  = "<div class='item-margin'></div>";
+        $item .= "<div class='item'>";
+        $item .= "<div class='item-heading'>";
+        $item .= "<img class='symbol' src='../../img/research/$c[2].png'>";
+        $item .= "<div class='achievement-heading'>$c[3]</div>";
+        $item .= "</div>";
+        $item .= "<div class='item-desc'>";
+        $item .= $c[4];
+        $item .= "</div>";
+        $item .= "<div class='item-thumbnail'>";
+        $item .= "<img src='$c[5]'>";
+        $item .= "</div>";
+
+        $item .= "<div class='achievement-list'>";
+        $json_arr = json_decode( $c[6] );
+        foreach( $json_arr as $obj ){
+            $name  = $obj->{"name"};
+            $label = $obj->{"label"};
+            $url   = $obj->{"url"};
+
+            $item .= "<div class='achievement-box'>";
+            $item .= "<a href='$url'>";
+            $item .= "<img src='../../../img/achievement/$name.jpg'>";
+            $item .= "<p>$label</p>";
+            $item .= "</a>";
+            $item .= "</div>";
+        }
+        $item .= "</div>";
+        $item .= "</div>";
+
+        echo $item;
+    }
+}
+
+?>
