e1fc3f403e1ae81597f204d9b0c46e11a72ce69f
satoken912 <seat.soil.74196901@gmail.com>
2018-05-20 01:54:17 +0900

    Modify PHP variables format

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,15 @@
 $(function(){
     $(".research-category-box, .achievement-box").mouseover(function(){
+
         $(this).css("background-color","#4ECCBF");
         $(this).find("a").find("img").css("opacity", 0);
         $(this).find("a").find("p").css({"background-color":"#4ECCBF", "color":"white"});
+
     }).mouseout(function(){
+
         $(this).css("background-color","#F5D0A9");
         $(this).find("a").find("img").css("opacity", 0.5);
         $(this).find("a").find("p").css({"background-color":"white", "color":"black", "opacity":0.85});
+        
     });
 });
--- php/connectToMySQL.php
+++ php/connectToMySQL.php
@@ -1,31 +1,35 @@
 <?php
 
 function showResearches( $_connection, $_category ){
-    $mode = $_GET["mode"];
+    
+    $mode = "";
+    if( isset($_GET["mode"]) ){
+        $mode = $_GET["mode"];
+    }
 
     $q = "SELECT * FROM research WHERE category='$_category'";
 
-    foreach( $_connection->query( $q ) as $c ){
+    foreach( $_connection->query( $q ) as list($id, $date, $category, $title, $description, $thumbnail_url, $achievements, $status) ){
 
-        if( $c[7] == "private" && $mode != "admin" ){
+        if( $status == "private" && $mode != "admin" ){
             continue;
         }
 
         $item  = "<div class='item-margin'></div>";
         $item .= "<div class='item'>";
         $item .= "<div class='item-heading'>";
-        $item .= "<img class='symbol' src='../../img/research/$c[2].png'>";
-        $item .= "<div class='achievement-heading'>$c[3]</div>";
+        $item .= "<img class='symbol' src='../../img/research/$category.png'>";
+        $item .= "<div class='achievement-heading'>$title</div>";
         $item .= "</div>";
         $item .= "<div class='item-desc'>";
-        $item .= $c[4];
+        $item .= $description;
         $item .= "</div>";
         $item .= "<div class='item-thumbnail'>";
-        $item .= "<img src='$c[5]'>";
+        $item .= "<img src='$thumbnail_url'>";
         $item .= "</div>";
 
         $item .= "<div class='achievement-list'>";
-        $json_arr = json_decode( $c[6] );
+        $json_arr = json_decode( $achievements );
         foreach( $json_arr as $obj ){
             $name  = $obj->{"name"};
             $label = $obj->{"label"};
